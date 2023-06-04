90390aeaf2a6e2f0d2b75b02021fe722fb38020c
Nikolai Killer <nikolai.killer@stud.h-da.de>
2023-06-04 14:03:16 +0200

    value docs

--- baseline/calculate_distance.py
+++ baseline/calculate_distance.py
@@ -2,6 +2,10 @@ import numpy as np
 import math
 
 """ calculate the distance from a given rssi value based on the groundtruth data and curve fit """
+# RSSI0 value is -24.35
+# path loss exponent = 2.4949
+# reference distance = 0.038
+
 def calculate_rssi_value_from_distance(distance):
     return -24.35 - 10 * 2.4949 * np.log10(distance / 0.038)
 
