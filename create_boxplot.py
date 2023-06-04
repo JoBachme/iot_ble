a978cdf04bc203a81106857e6d491e3b9bebeded
Jörn Bachmeier <joern.bachmeier@student.hs-rm.de>
2023-06-04 21:02:29 +0200

    safety merge. Changed some txt files and paths

--- create_boxplot.py
+++ create_boxplot.py
@@ -1,14 +1,18 @@
 from matplotlib import pyplot as plt
+from pathlib import Path
 import json
 import os
 import numpy as np
 import pandas as pd
 
-DIRECTORY = "Messungen"
+THIS_FILE = Path(__file__).parent.resolve()
+DIR_TRAINING = THIS_FILE / "Messungen/1Messung_Training"
+DIR_TEST = THIS_FILE / "Messungen/1Messung_Test"
+DIR_2TRAINING = THIS_FILE / "Messungen/2Messung_Training"
+mess_abstaende = [0, 5, 10, 12.5, 15, 17.5, 20, 22, 24, 26, 27]
 
-def create_messung_dict(filename):
+def messung(filename):
     data_array = []
-    dataset = dict()
     count = 0
 
     with open(filename, "r", encoding="UTF-8") as file:
@@ -28,27 +32,38 @@ def create_messung_dict(filename):
     print("Median: ", np.median(data_array))
     print("Std: ", np.std(data_array))
     print(f"Untere Grenze: {data_array[0]}, Obere Grenze: {data_array[-1]}")
-
-    # plt.boxplot(dataset)
-    # plt.show()
+    print()
 
     return data_array
+
+def create_messung_dict(DIRECTORY):
+    data = dict()
+    for filename in os.listdir(DIRECTORY):
+        f = os.path.join(DIRECTORY, filename)
+        print(f)
+        arr = messung(f)
+        f = int(f.split("_")[-2][:-2])
+        data[f] = arr
+    return data
 
 plt.rcParams["figure.figsize"] = [10, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-data = dict()
+data = create_messung_dict(DIR_TRAINING)
+test = create_messung_dict(DIR_TEST)
 fig, ax = plt.subplots()
 
-for filename in os.listdir(DIRECTORY):
-    f = os.path.join(DIRECTORY, filename)
-    arr = create_messung_dict(f)
-    f = int(f.split("_")[1][:-2])
-    data[f] = arr
-
 data = dict(sorted(data.items()))
+test = dict(sorted(test.items()))
 ax.boxplot(data.values())
 ax.set_xticklabels(data.keys())
+
+plt.plot(5, -40, marker="o", markersize=5, markeredgecolor="red")
+
+for arr in list(zip(list(test.values()), mess_abstaende)):
+    y = np.median(arr[0])
+    x = arr[1]+1
+    plt.plot(x, y, marker="o", markersize=5, markeredgecolor="red")
 
 plt.show()
 
