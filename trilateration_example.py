629b6ce870009a909d7797a942dd3b7caa59051e
Jörn Bachmeier <joern.bachmeier@student.hs-rm.de>
2023-06-05 12:46:33 +0200

    trilateration is working correctly

--- trilateration_example.py
+++ trilateration_example.py
@@ -1,32 +1,43 @@
 import numpy as np
+from scipy.optimize import minimize
 
 def trilateration(ref_points, distances):
     num_points = len(ref_points)
+    
+    def distance_error(point):
+        error = 0
+        for i in range(num_points):
+            distance_i = np.linalg.norm(ref_points[i] - point[:3])
+            error += (distance_i - distances[i]) ** 2
+        return error
 
-    # Construct the matrix A and vector b for solving the equations Ax = b
-    A = np.zeros((num_points - 1, 3))
-    b = np.zeros((num_points - 1, 1))
-    for i in range(num_points - 1):
-        A[i, :] = 2 * (ref_points[i + 1] - ref_points[0])
-        b[i] = np.linalg.norm(ref_points[0]) ** 2 - np.linalg.norm(ref_points[i + 1]) ** 2 - distances[0] ** 2 + distances[i + 1] ** 2
+    # Perform optimization to minimize the distance error
+    initial_guess = np.mean(ref_points, axis=0)  # Initial guess for the unknown point
+    result = minimize(distance_error, initial_guess, method='Nelder-Mead')
 
-    # Solve the system of equations using least squares optimization
-    x = np.linalg.lstsq(A, b, rcond=None)[0]
-
-    # Calculate the coordinates of the unknown point
-    unknown_point = x.flatten()
+    # Retrieve the optimized unknown point coordinates
+    unknown_point = result.x[:3]
 
     return unknown_point
 
+def calculate_distance(point1, point2):
+    point1 = np.array(point1)
+    point2 = np.array(point2)
+    distance = np.linalg.norm(point2 - point1)
+    return distance
+
 # Example usage
 # Define the known reference points and distances
-ref_points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]])  # Coordinates of reference points
-distances = np.array([2, 2, 2, 2])  # Distances from the reference points to the unknown point
+ref_points = np.array([[0, 0, 0], [100, 0, 0], [0, 100, 0], [100, 100, 0]])  # Coordinates of reference points
+distances = np.array([(np.sqrt(2)/2)*50, (np.sqrt(2)/2)*50, (np.sqrt(2)/2)*50, (np.sqrt(2)/2)*50])  # Distances from the reference points to the unknown point
+print(distances)
 
 # Calculate trilateration
 unknown_point = trilateration(ref_points, distances)
+unknown_point = [np.round(x, 3) for x in unknown_point]
+
+for ref in ref_points:
+    print(calculate_distance(ref, unknown_point))
 
 # Print the coordinates of the unknown point
 print("Coordinates of the unknown point:", unknown_point)
-
-#TODO We need a function to get a distance from the reference point to the unknown point with the RSSI value
